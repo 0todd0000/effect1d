4f8cec9b86ea902f85a741e7f5be603a4e5efeb1
0todd0@gmail.com <0todd0@gmail.com>
2024-12-17 21:06:57 +0900

    try33

--- content/e1dwidgets/basic.py
+++ content/e1dwidgets/basic.py
@@ -56,7 +56,7 @@ class MPLWidget( widgets.Stack ):
     def update(self, params, mgr):
         
         with self.out:
-            clear_output(wait=True)
+            clear_output(wait=False)
             self.ax.cla()
             if self.basic:
                 import numpy as np
