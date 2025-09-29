af11114cd52708bcc2084c8637ee5ac406e9d6a2
Todd Pataky <0todd0@gmail.com>
2025-09-29 12:32:25 +0900

    update oa fig

--- src/Scripts/Bertaux2022/2_analysis.py
+++ src/Scripts/Bertaux2022/2_analysis.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 import spm1d
-import rft1d
+
 
 
 def unique_sorted(x):
@@ -21,7 +21,7 @@ def d2t_onesample(d, n):
 def d2p_onesample_0d(d, n):
     '''
     Calculate probabilty associated with Cohen's d value for the
-    two-sample case with group sizes of n 
+    one-sample (or paired) case with a sample sizes of n
     '''
     from collections.abc import Iterable
     from scipy import stats
@@ -34,12 +34,14 @@ def d2p_onesample_0d(d, n):
     
 
 def d2p_onesample_1d(d, n, Q, fwhm):
+    import rft1d
     v  = n - 1
     u  = d2t_onesample(d, n)
     p  = rft1d.t.sf(u, v, Q, fwhm)
     return p
 
 def p2d_onesample_1d(p, n, Q, fwhm):
+    import rft1d
     v  = n - 1
     t  = rft1d.t.isf(p, v, Q, fwhm)
     d  = t2d_onesample(t, n)
@@ -61,68 +63,35 @@ with h5py.File(fpathH5, 'r') as f:
 
 
 
+# separate data into groups (right limb only;  results are similar for left-limb only)
+# group 0:  healthy (group=0)
+# group 1:  OA, month 0 (group=1, sess=0)
+# group 2:  OA, month 6 (group=1, sess=1)
+limb   = 1
+y0     = d['y'][  (d['group']==0) & (d['limb']==limb) ]
+oasubj = unique_sorted( d['subj'][(d['affected_limb']==limb) & d['sess']==1] )
+y1     = np.vstack([d['y'][  (d['subj']==u) & (d['limb']==limb) & (d['sess']==0) ]  for u in oasubj])
+y2     = np.vstack([d['y'][  (d['subj']==u) & (d['limb']==limb) & (d['sess']==1) ]  for u in oasubj])
 
 
-limb  = 1
-
-y0    = d['y'][  (d['group']==0) & (d['limb']==limb) ]
-usubj = unique_sorted( d['subj'][(d['affected_limb']==limb) & d['sess']==1] )
-y1    = np.vstack([d['y'][  (d['subj']==u) & (d['limb']==limb) & (d['sess']==0) ]  for u in usubj])
-y2    = np.vstack([d['y'][  (d['subj']==u) & (d['limb']==limb) & (d['sess']==1) ]  for u in usubj])
+# conduct functional hypothesis test using SPM
+spm    = spm1d.stats.ttest_paired( y2, y1 ).inference(0.05)
+fwhm   = spm.fwhm
 
 
-# # find roughest:
-# y     = y2 - y1     # pairwise differences
-# r     = y - y.mean(axis=0)
-# dydt  = (np.diff(r, axis=1)**2).sum(axis=1)
-# # dydt  = r.std(axis=1)
-# ind   = np.argsort(dydt)[::-1]
-#
-# nsubj = 10
-# ind   = ind[:nsubj]
-#
-# y1    = y1[ind]
-# y2    = y2[ind]
-
-
-
-# nsubj = 10
-# np.random.seed(0)
-# ind   = np.random.permutation( y1.shape[0] )[:nsubj]
-# y1    = y1[ind]
-# y2    = y2[ind]
-
-
-# nsubj  = 15
-# y1,y2  = y1[:nsubj], y2[:nsubj]
-# # y1,y2  = y1[-nsubj:], y2[-nsubj:]
-
-
-
-spm   = spm1d.stats.ttest_paired( y2, y1 ).inference(0.05)
-fwhm  = spm.fwhm
-
-
-# calculate effect size:
+# calculate effect size and their probabilities:
 y     = y2 - y1     # pairwise differences
-
-
-
 n,Q   = y.shape     # sample size, domain size
-d     = y.mean(axis=0) / y.std(ddof=1, axis=0)
-t     = d2t_onesample(d, n)
-
-
-
+d     = y.mean(axis=0) / y.std(ddof=1, axis=0)  # functional Cohen's d value
+t     = d2t_onesample(d, n)  # functional t-value (also calculated above using spm1d.stats.ttest_paired;  this is just a check)
 p0    = d2p_onesample_0d(-d, n)
 p1    = d2p_onesample_1d(-d, n, Q, fwhm)
 
 
-
+# calculate interpretations:
 labels = ('Very small', 'Small', 'Medium', 'Large', 'Very large', 'Huge')
 dth0   = (0.01, 0.2, 0.5, 0.8, 1.2, 2.0)  # d-value thresholds for 0D case
-nn     = 10
-ww     = 25
+nn,ww  = 10, 25   # approximations from recommended guidelines;  see paper for a discussion
 pth    = d2p_onesample_0d( dth0, nn )
 dth1   = p2d_onesample_1d( pth, nn, Q, ww )
 
@@ -130,47 +99,49 @@ print( fwhm )
 print( d.min() )
 print( dth0 )
 print( np.around(dth1,3) )
-# dth1   = p2t_onesample_1d( pth, n )
-
-# v      = n - 1
-# tth1   = rft1d.t.isf(pth, v, Q, fwhm)
-
-# print( tth1 )
 
 
 
-
-
-
-
+# plot:
 plt.close('all')
 fig,axs = plt.subplots( 2, 2, figsize=(8,6), tight_layout=True )
 ax0,ax1,ax2,ax3 = axs.ravel()
-# ax0.plot( y0.T, color='k', lw=0.2 )
+
 ax0.plot( y1.T, color='r', lw=0.2 )
 ax0.plot( y2.T, color='c', lw=0.2 )
-
-ax0.plot( y0.mean(axis=0), color='k', lw=5 )
-ax0.plot( y1.mean(axis=0), color='r', lw=5 )
-ax0.plot( y2.mean(axis=0), color='c', lw=5 )
+ax0.plot( y0.mean(axis=0), color='k', lw=5, label='Healthy' )
+ax0.plot( y1.mean(axis=0), color='r', lw=5, label='OA, Month 0' )
+ax0.plot( y2.mean(axis=0), color='c', lw=5, label='OA, Month 6' )
+ax0.legend()
+ax0.set_xlabel('Time (%)')
+ax0.set_ylabel('Hip flexion (deg)')
 
 
 
 spm.plot( ax=ax1 )
-
 ax1.plot( t, 'r')
+ax1.set_xlabel('Time (%)', size=10)
+ax1.set_ylabel('t-value', size=10)
 
 
 ax2.plot( d )
-for dd0,dd1,ss in zip(dth0,dth1,labels):
+for dd0,dd1,ss in zip(dth0,dth1,labels[:3]):
     ax2.axhline(-dd0, color='k', linestyle=':')
     ax2.axhline(-dd1, color='r', linestyle=':')
+ax2.set_xlabel('Time (%)', size=10)
+ax2.set_ylabel('Effect size (d-value)', size=10)
 
 
-ax3.plot( p0, label='0d' )
-ax3.plot( p1, label='1d' )
+
+# ax3.plot( p0, label='Uncorrected' )
+# ax3.plot( p1, label='RFT-corrected' )
+ax3.plot( np.log(p0), label='Uncorrected' )
+ax3.plot( np.log(p1), label='RFT-corrected' )
 ax3.legend()
-ax3.set_ylim(0, 0.2)
+# ax3.set_ylim(0, 0.2)
+ax3.set_xlabel('Time (%)', size=10)
+ax3.set_ylabel('log-probability', size=10)
+
 
 plt.show()
 
